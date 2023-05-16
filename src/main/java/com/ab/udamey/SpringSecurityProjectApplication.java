0ae90b9cf09c5607b944586bc27258fba268ad83
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2023-05-16 16:54:01 +0530

    Project Created

---
+++ src/main/java/com/ab/udamey/SpringSecurityProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ab.udamey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProjectApplication.class, args);
+		System.out.println("Spring Boot Application is Running");
+	}
+
+}
